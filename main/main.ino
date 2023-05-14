3c05ab9cb26b9e479f0063c18b94890058369b02
Kaio Sodré <kaio.sodc@gmail.com>
2023-05-14 16:49:17 -0300

    Wifi esp8266 config

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,38 @@
-const int LED_PIN = 2;
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+ESP8266WebServer server(80);
+
+// WiFi Esp8266 Config
+IPAddress localIp(192,168,1,1);
+IPAddress gatewayIp(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+const char *ssid="esp8266_swa";
+const char *pwd="swa12345678";
+
+// Home Page
+void handle_home_page() {
+  String html = "<html><body><h1>Hello, world!</h1></body></html>";
+  server.send(200, "text/html", html);
+}
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
+  // Setup Wifi
+  Serial.begin(9600);
+  Serial.println("Inicializando AP...");
+  if (WiFi.softAP(ssid, pwd)) {
+    Serial.println("AP disponivel....");
+    WiFi.softAPConfig(localIp, gatewayIp, subnet);
+    delay(200);
+  }
+  Serial.println("Wifi AP disponivel...");
+  server.on("/", handle_home_page);
+  // server.on("/led/on", handle_led_on);
+  // server.on("/led/off", handle_led_off);
+  server.begin();
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
+  server.handleClient();
 }
