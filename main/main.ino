18f56116490c07941020cc4af86c36d361ebe38a
Kaio Sodré <kaio.sodc@gmail.com>
2023-05-28 21:08:03 -0300

    edit main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,54 +1,156 @@
-#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <FS.h>
+
+// O IP padrão é o 192.168.4.1
+const char* ssid = "KickBall"; // Nome da rede Wi-Fi do Access Point
+const char* password = "KB102030"; // Senha para o Access Point
+
+const int ledPin = 2; // Led do ESP8266
+bool ledState = false; // Estado atual do LED
 
 ESP8266WebServer server(80);
 
-// Exemplo 
-const int LED_PIN = 2;
+const char* html = R"html(
+<html> 
+  <head>   
+    <title>KickBall</title> 
+    <style>
+      #title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 30px;
+      }
 
-// WiFi Esp8266 Config
-IPAddress localIp(192,168,1,1);
-IPAddress gatewayIp(192,168,1,1);
-IPAddress subnet(255,255,255,0);
-const char *ssid="KickBall";
-const char *pwd="kgb102030";
+      .joystick {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding-top: 130px;
+      }
 
-// Home Page
-void handle_home_page() {
-  String html = "<html><body><h1>Hello, world!</h1></body></html>";
-  // File file = SPIFFS.open("/index.html", "r");
-  // String html = file.readString();
+      .button-lr {
+        background-color: yellow;
+        font-size: 20px;
+        width: 200px;
+        height: 200px;
+        margin-right: 50px;
+        padding: 50px;
+        padding-left: 10px;
+        padding-right: 10px;
+        border-radius: 50%;
+        
+      }
+
+      #button-hit{
+        background-color: red;
+        font-size: 20px;
+        width: 200px;
+        height: 200px;
+        margin-right: 50px;
+        padding: 50px;
+        margin-left: 70px;
+        padding-left: 10px;
+        padding-right: 10px;
+        border-radius: 50%;
+      }
+    @media (max-width: 500px){
+      #title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 30px;
+      }
+
+      .joystick {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding-top: 130px;
+      }
+
+      .button-lr {
+        background-color: yellow;
+        font-size: 20px;
+        width: 200px;
+        height: 200px;
+        margin-right: 50px;
+        padding: 50px;
+        padding-left: 10px;
+        padding-right: 10px;
+        border-radius: 50%;
+        
+      }
+
+      #button-hit{
+        background-color: red;
+        font-size: 20px;
+        width: 200px;
+        height: 200px;
+        margin-right: 50px;
+        padding: 50px;
+        margin-left: 70px;
+        padding-left: 10px;
+        padding-right: 10px;
+        border-radius: 50%;
+      }
+    }
+    </style>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>  
+   </head>  
+   <body> 
+
+    <header>
+      <div>
+        <h1 id="title">Kick Ball Game</h1>
+      </div>
+    </header>
+    
+    <div class="joystick">
+      <a href="http://192.168.4.1/on">
+        <button class="button-lr">Direita</button>
+      </a>
+      <a href="http://192.168.4.1/off">
+        <button class="button-lr">Esquerda</button>
+      </a>
+      <a href="#">
+        <button id="button-hit">Chute</button>
+      </a>
+    </div>
+
+    </body>  
+</html>
+)html";
+
+void handleRoot() {
   server.send(200, "text/html", html);
 }
 
-// Exemplo
-void handle_led_on() {
-  digitalWrite(LED_PIN, HIGH);
+void handleOn() {
+  ledState = true;
+  digitalWrite(ledPin, HIGH);
+  server.sendHeader("Location", "/");
+  server.send(303);
 }
 
-//Exemplo
-void handle_led_off() {
-  digitalWrite(LED_PIN, LOW);
+void handleOff() {
+  ledState = false;
+  digitalWrite(ledPin, LOW);
+  server.sendHeader("Location", "/");
+  server.send(303);
 }
 
 void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
 
-  // Exemplo
-  pinMode(LED_PIN, OUTPUT);
+  WiFi.softAP(ssid, password);
 
-  // Setup Wifi
-  Serial.begin(9600);
-  Serial.println("Inicializando AP...");
-  if (WiFi.softAP(ssid, pwd)) {
-    Serial.println("AP disponivel....");
-    WiFi.softAPConfig(localIp, gatewayIp, subnet);
-    delay(200);
-  }
-  Serial.println("Wifi AP disponivel...");
-  server.on("/", handle_home_page);
-  server.on("/led/on", handle_led_on);
-  server.on("/led/off", handle_led_off);
+  server.on("/", handleRoot);
+  server.on("/on", handleOn);
+  server.on("/off", handleOff);
+
   server.begin();
 }
 
